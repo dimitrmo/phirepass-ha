149cdc9ebfd594eeda7ae0226cf5109895280427
dimitrmo <dmouratidis153@gmail.com>
2026-02-12 22:29:45 +0200

    Print env

--- phirepass-daemon-ha/rootfs/app/run.sh
+++ phirepass-daemon-ha/rootfs/app/run.sh
@@ -41,6 +41,8 @@ echo "Starting SSH server on ${SSH_HOST}:${SSH_PORT}..."
 
 echo "Running phirepass agent..."
 
+env
+
 echo $PAT_TOKEN | /app/agent login --from-stdin --server-host $SERVER_HOST --server-port $SERVER_PORT
 
 exec /app/agent start
